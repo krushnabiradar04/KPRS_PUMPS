e84b9a4e14b2118634ff771745a91cb12edae968
Sahas2004 <sahaskshirsagar2004@gmail.com>
2025-08-05 15:29:58 +0530

    changes

--- src/NavBar/Navbar.jsx
+++ src/NavBar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
-import "./Navbar.css"
 import { ScrollToTop } from "../ScroolToTop";
+import './NavBar.css'
 
 export const Navbar = () => {
   const location = useLocation();
